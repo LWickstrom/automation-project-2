4bfd6d0710ec25dbeb63c41a8e894e00a9ebfdfc
LWickstrom <wickstrom.ljubov@gmail.com>
2023-07-17 19:20:42 +0300

    Update issue-delete.cy.js

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,69 +1,75 @@
 describe('Issue create', () => {
     beforeEach(() => {
-      cy.visit('/');
-      cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-      //System will already open issue creating modal in beforeEach block  
-      cy.visit(url + '/board?modal-issue-create=true');
-      });
+        cy.visit('/');
+        cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+            // open first available issue
+            cy.get('[data-testid="board-list:backlog"]').children().eq(0).click();
+        });
     });
-  
-    it('Should create an issue and validate it successfully', () => {
-      //System finds modal for creating issue and does next steps inside of it
-      cy.get('[data-testid="modal:issue-create"]').within(() => {
-        
-        //open issue type dropdown and choose Story
-        cy.get('[data-testid="select:type"]').click();
-        cy.get('[data-testid="select-option:Story"]')
-            .trigger('click');
-              
-        //Type value to description input field
-        cy.get('.ql-editor').type('TEST_DESCRIPTION');
-  
-        //Type value to title input field
-        //Order of filling in the fields is first description, then title on purpose
-        //Otherwise filling title first sometimes doesn't work due to web page implementation
-        cy.get('input[name="title"]').type('TEST_TITLE');
-        
-        //Select Lord Gaben from reporter dropdown
-        cy.get('[data-testid="select:userIds"]').click();
-        cy.get('[data-testid="select-option:Lord Gaben"]').click();
-  
-        //Click on button "Create issue"
-        cy.get('button[type="submit"]').click();
-      });
-  
-      //Assert that modal window is closed and successful message is visible
-      cy.get('[data-testid="modal:issue-create"]').should('not.exist');
-      cy.contains('Issue has been successfully created.').should('be.visible');
-      
-      //Reload the page to be able to see recently created issue
-      //Assert that successful message has dissappeared after the reload
-      cy.reload();
-      cy.contains('Issue has been successfully created.').should('not.exist');
-  
-      //Assert than only one list with name Backlog is visible and do steps inside of it
-      cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-        //Assert that this list contains 5 issues and first element with tag p has specified text
-        cy.get('[data-testid="list-issue"]')
-            .should('have.length', '5')
-            .first()
-            .find('p')
-            .contains('TEST_TITLE');
-        //Assert that correct avatar and type icon are visible
-        cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
-        cy.get('[data-testid="icon:story"]').should('be.visible');
-      });
+
+    // Assignment 3. Test 1
+    it('Delete first issue and verify it has been successfully removed from board', () => {
+
+        let issueTitle
+
+        //Getting issue details modal and working with it.
+        cy.get('[data-testid="modal:issue-details"]').as('issueModal').should('be.visible');
+        cy.get('@issueModal').within(() => {
+
+            cy.get('[placeholder="Short summary"]').invoke('text').then((title) => {
+                issueTitle = title;
+                cy.log(`Deleting issue: "${issueTitle}"`)
+            })
+
+            cy.get('[data-testid="icon:trash"]').click()
+
+        }).then(() => {
+
+            // only then working with confimation modal and board list, preventing async execusion 
+            cy.get('[data-testid="modal:confirm"]').contains('Delete issue').click()
+            cy.get('[data-testid="modal:confirm"]').should('not.exist')
+
+            cy.reload()
+
+            // assert target issue is deleted and not visible on the jira board
+            cy.get('[data-testid="board-list:backlog"]').children().should('not.contain', issueTitle);
+
+        });
+
     });
-  
-    it('Should validate title is required field if missing', () => {
-      //System finds modal for creating issue and does next steps inside of it
-      cy.get('[data-testid="modal:issue-create"]').within(() => {
-        //Try to click create issue button without filling any data
-        cy.get('button[type="submit"]').click();
-  
-        //Assert that correct error message is visible
-        cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
-      });
+
+    // Assignment 3. Test 2
+    it('Start issue delete process and abort it. Verify issue is not affected', () => {
+
+        let issueTitle
+
+        //Getting issue details modal and working with it.
+        cy.get('[data-testid="modal:issue-details"]').as('issueModal').should('be.visible');
+        cy.get('@issueModal').within(() => {
+
+            cy.get('[placeholder="Short summary"]').invoke('text').then((title) => {
+                issueTitle = title;
+                cy.log(`Deleting issue: "${issueTitle}"`)
+            })
+
+            cy.get('[data-testid="icon:trash"]').click()
+
+        }).then(() => {
+
+            // only then working with confimation modal and board list, preventing async execusion 
+            cy.get('[data-testid="modal:confirm"]').contains('Cancel').click()
+            cy.get('[data-testid="modal:confirm"]').should('not.exist')
+
+            cy.reload()
+
+            cy.get('@issueModal').within(() => {
+                cy.get('[data-testid="icon:close"]').first().click();
+            })
+            // assert target issue is not deleted and visible on the jira board
+            cy.get('[data-testid="board-list:backlog"]').children().should('contain', issueTitle);
+
+        });
+
     });
-  });
-  
+
+});
